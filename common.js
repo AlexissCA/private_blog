444928ea525b1cefdabd7f665509dcb94f6d8eee
AlexissCode <andrewblow@gmail.com>
2024-04-12 16:29:16 +0200

    layout base

---
+++ common.js
@@ -0,0 +1,42 @@
+const Error_MSGS_LOGIN = {
+	"Email": ["Email address is required", "Enter a valid email address"],
+	"Email backend": ["Email address doesn't match any account"],
+	"Password": ["Password is required"],
+	"Password backend": ["Password is incorrect"],
+};
+
+const Error_MSGS_REGISTRATION = {
+	"Email": ["Email address is required", "Enter a valid email address"],
+	"Email backend": ["Email address doesn't match any invited account", "Email address is already registered"],
+	"Username": ["Username is required", "Enter at least 3 non-empty characters"],
+	"Username backend": ["Username is already taken"],
+	"Password": ["Password is required", "Enter a valid password as specified above"],
+	"Password backend": [""],
+	"Confirm password": ["Password confirmation is required", "Passwords do not match", "Password is required"],
+	"Confirm password backend": [""],
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
